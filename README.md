efedb97d7d4145b9fd1232e153abadd55de09e1c
Sebastian Katszer <sebastian.katszer@gmail.com>
2015-10-24 12:39:35 +0200

    libGDX system

--- README.md
+++
@@ -1,4 +0,0 @@
-﻿# Wildfire
-Symulacja pożaru lasu
-
-Znaleźć artykuły, zsyntezować i zaproponować swoje rozwiązanie, dodać bibliotekę.
---
+++ desktop/src/wildfire/simulation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package wildfire.simulation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import wildfire.simulation.Wildfire;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Wildfire(), config);
+	}
+}
